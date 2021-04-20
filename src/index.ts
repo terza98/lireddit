7f3c76f7008687b655cb98b5e94150f636afc67b
terza98 <terza98@hotmail.rs>
2021-04-20 20:44:20 +0200

    create project and set up mikro orm

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const post = orm.em.create(Post, { title: "My first post" });
+  await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((err) => console.log(err));
+
+console.log("Hello World!");
